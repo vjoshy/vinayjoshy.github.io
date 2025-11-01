369cf3a298195138b15383936f6a9dd816a0bf9e
vjoshy <80487875+vjoshy@users.noreply.github.com>
2025-11-01 02:29:48 +0000

    Deploying to gh-pages from @ vjoshy/vjoshy.github.io@8913d0c5ca3afc5f83dbae7354e4eec927620cf8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vjoshy/vinayjoshy.github.io",
+    "data-repo": "vjoshy/vjoshy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
